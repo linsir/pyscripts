6bc4f64f4e11c4d602e74b309fdf0d0e9c5bd61b
Linsir <vi5i0n@hotmail.com>
2015-01-13 09:26:38 +0800

    add sendmail.py

---
+++ sendmail.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#coding: utf-8
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+sender = 'sender@163.com'
+receiver = 'rece@qq.com'
+subject = 'python email test'
+smtpserver = 'smtp.163.com'
+username = 'username'
+password = 'password'
+
+msg = MIMEText('你好','plain','utf-8')#中文需参数‘utf-8’，单字节字符不需要
+msg['Subject'] = Header(subject, 'utf-8')
+
+smtp = smtplib.SMTP()
+smtp.connect('smtp.163.com')
+smtp.login(username, password)
+smtp.sendmail(sender, receiver, msg.as_string())
+smtp.quit()

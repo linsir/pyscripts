37cd285a6c65c59db77e8dfbeef153eb710350f6
Linsir <vi5i0n@hotmail.com>
2015-07-04 11:51:58 +0800

    支持多个文件

--- pyzip.py
+++ pyzip.py
@@ -6,17 +6,19 @@ import sys
 import zipfile
 
 #print "Processing File " + sys.argv[1]
-
-file=zipfile.ZipFile(sys.argv[1],"r");
-for name in file.namelist():
-    utf8name=name.decode('gbk')
-#    print "Extracting " + utf8name
-    pathname = os.path.dirname(utf8name)
-    if not os.path.exists(pathname) and pathname!= "":
-        os.makedirs(pathname)
-    data = file.read(name)
-    if not os.path.exists(utf8name):
-        fo = open(utf8name, "w")
-        fo.write(data)
-        fo.close
-file.close()
+list = sys.argv[1:]
+for filename in list:
+    print filename
+    file=zipfile.ZipFile(filename,"r");
+    for name in file.namelist():
+        utf8name=name.decode('gbk')
+    #    print "Extracting " + utf8name
+        pathname = os.path.dirname(utf8name)
+        if not os.path.exists(pathname) and pathname!= "":
+            os.makedirs(pathname)
+        data = file.read(name)
+        if not os.path.exists(utf8name):
+            fo = open(utf8name, "w")
+            fo.write(data)
+            fo.close
+    file.close()

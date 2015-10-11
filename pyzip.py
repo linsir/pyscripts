52808d3d19c04ea8db341abc7449bd256069324d
Linsir <vi5i0n@hotmail.com>
2015-10-12 01:30:07 +0800

    若根不是文件夹则自动创建

--- pyzip.py
+++ pyzip.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+'''
+说明：解决解压zip中文件名乱码
+安装：sudo cp pyzip.py /usr/bin/pyzip && sudo chmod +x /usr/bin/pyzip
+用法：pyzip filename
+'''
 import os
 import sys
 import zipfile
 
-#print "Processing File " + sys.argv[1]
+
 list = sys.argv[1:]
 for filename in list:
-    print filename
-    file=zipfile.ZipFile(filename,"r");
-    for name in file.namelist():
-        utf8name=name.decode('gbk')
-    #    print "Extracting " + utf8name
+    print "Processing File: %s" %filename
+    file = zipfile.ZipFile(filename,"r");
+    file_list = file.namelist()
+    if file_list[0].endswith('/') and (file_list[0] == file_list[-1]):
+        pre_folder = ''
+    else:
+        pre_folder = filename.strip('.zip') + '/'
+
+    for name in file_list:
+        utf8name = pre_folder + name.decode('gbk')
+        print "Extracting %s" %utf8name 
         pathname = os.path.dirname(utf8name)
         if not os.path.exists(pathname) and pathname!= "":
             os.makedirs(pathname)
@@ -22,3 +32,4 @@ for filename in list:
             fo.write(data)
             fo.close
     file.close()
+print 'Extract Sucess!'

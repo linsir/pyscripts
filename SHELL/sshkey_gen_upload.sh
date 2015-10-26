5c4d433ef2f7be256078ff22f1abef499d4bf895
Linsir <vi5i0n@hotmail.com>
2015-10-26 11:09:49 +0800

    sshkey gen

---
+++ SHELL/sshkey_gen_upload.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+clear
+
+echo "# auto gen ssh key  and upload to remote server "
+echo "# Author:linsir"
+echo "# linsir.org"
+echo ""
+
+# sshkey gen
+read -p "pls input a note for key:" note
+echo $note 
+
+if [ "$note" = "" ];then
+    exit 
+    # ssh-keygen -t rsa
+else
+    if [ -f ~/.ssh/$note ];then
+        echo "the key exist!use old key"
+    else
+        ssh-keygen -t rsa -C $note -f ~/.ssh/$note
+    fi
+fi
+
+# copy the key to remote server
+
+read -p "pls input username of server:" user
+read -p "pls input ip adderss of server:" ip
+read -p "pls input sshd port of server:" port
+
+if [ "$port" = "" ];then
+    port=22
+fi
+if [ "$user" = "" ] && [ "$ip" = "" ];then
+    exit 
+else
+    ssh-copy-id -i ~/.ssh/$note.pub $user@$ip:$port
+fi
+
+# config ssh config
+
+ cat >> ~/.ssh/config<<-EOF
+Host $note
+    hostname $ip
+    user $user
+    port $port
+    IdentityFile ~/.ssh/$note
+EOF
+ssh-add ~/.ssh/$note
+exit

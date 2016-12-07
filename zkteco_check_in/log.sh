c61f57870a5676247138be82625ae85b4c3d2ceb
vi5i0n <vi5i0n@hotmail.com>
2016-12-07 16:43:09 +0800

    add change finger

--- zkteco_check_in/log.sh
+++ zkteco_check_in/log.sh
@@ -2,7 +2,7 @@
 # @Date    : 2016-06-07 16:04:54
 # @Author  : Linsir (root@linsir.org)
 # @Link    : http://linsir.org
-# @Version :
+# @Version : 0.2
 # 2008 2010 2055
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
@@ -20,6 +20,11 @@ function del_log(){
     log_id=$1
     old_time=`./sqlite3_mips ZKDB.db "SELECT Verify_Time FROM ATT_LOG WHERE ID = '${log_id}';"`
     user_id=`./sqlite3_mips ZKDB.db "SELECT User_PIN FROM ATT_LOG WHERE ID = '${log_id}';"`
+    if [ "${user_id}" = ""]; then
+        echo -e "[\033[32;1mError\033[0m]: Bad [log_id], please try again"
+        menu
+        exit
+    fi
     name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${user_id}'"`
     echo -e "Are you sure delete the log: [\033[32;1m${name}: ${old_time}\033[0m] "
     read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
@@ -32,6 +37,11 @@ function query_id(){
     echo "==========================="
     name=$1
     user_id=`./sqlite3_mips ZKDB.db "SELECT User_PIN FROM USER_INFO WHERE NAME = '${name}'"`
+    if [ "${user_id}" = ""]; then
+        echo -e "[\033[32;1mError\033[0m]: Bad [name], please try again"
+        menu
+        exit
+    fi
     echo -e "[\033[32;1m${name}\033[0m]'s ID is [\033[32;1m${user_id}\033[0m] "
     ./sqlite3_mips ZKDB.db ".quit"
 }
@@ -40,6 +50,11 @@ function check_in(){
     echo "==========================="
     user_id=$1
     name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${user_id}'"`
+    if [ "${name}" = ""]; then
+        echo -e "[\033[32;1mError\033[0m]: Bad [user_id], please try again"
+        menu
+        exit
+    fi
     echo -e "签到用户: [\033[32;1m${name}\033[0m]"
 
     today=$(date '+%Y-%m-%d')
@@ -65,6 +80,11 @@ function change_log(){
         user_id=2055
     fi
     name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${user_id}'"`
+    if [ "${name}" = ""]; then
+        echo -e "[\033[32;1mError\033[0m]: Bad [user_id], please try again"
+        menu
+        exit
+    fi
     echo -e "签到用户: [\033[32;1m${name}\033[0m] (${user_id})"
     Month=$(date '+%Y-%m%')
     
@@ -87,6 +107,11 @@ function change_log(){
         DateTime=$1
     fi
     old_time=`./sqlite3_mips ZKDB.db "SELECT Verify_Time FROM ATT_LOG WHERE ID = '${ID}';"`
+    if [ "${old_time}" = ""]; then
+        echo -e "[\033[32;1mError\033[0m]: Bad [log_id], please try again"
+        menu
+        exit
+    fi
     echo -e "Are you sure to change [\033[32;1m${old_time}\033[0m] into [\033[32;1m${DateTime}\033[0m]?"
     read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
 
@@ -94,6 +119,50 @@ function change_log(){
     echo -e "[\033[32;1mChange Sucessfuly\033[0m]"
     # ./sqlite3_mips ZKDB.db "SELECT ID,User_PIN,Verify_Time FROM ATT_LOG WHERE ID = '${ID}';"
     ./sqlite3_mips ZKDB.db ".quit"
+}
+
+function change_finger(){
+    echo "==========================="
+    old_user_id=$1
+    new_user_id=$2
+    if [ "${old_user_id}" = "" -a "${new_user_id}" = "" ]; then
+        echo -e "[\033[32;1mError\033[0m]: Bad option, please choose again"
+        menu
+        exit
+    fi
+    old_name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${old_user_id}'"`
+    new_name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${new_user_id}'"`
+    if [ "${old_name}" = "" -a "${new_name}" = "" ]; then
+        echo -e "[\033[32;1mError\033[0m]: Bad [ID], please try again"
+        menu
+        exit
+    fi
+    old_user_defaulf_id=`./sqlite3_mips ZKDB.db "SELECT ID FROM USER_INFO WHERE User_PIN = '${old_user_id}'"`
+    new_user_defaulf_id=`./sqlite3_mips ZKDB.db "SELECT ID FROM USER_INFO WHERE User_PIN = '${new_user_id}'"`
+    finger_id=`./sqlite3_mips ZKDB.db "SELECT ID FROM fptemplate10 WHERE pin = '${old_user_defaulf_id}' limit 1"`
+    if [ "${finger_id}" = "" ]; then
+        echo -e "[\033[32;1mError\033[0m]: the user [\033[32;1m${old_name}\033[0m] have no finger data, please try again"
+        menu
+        exit
+    fi
+    finger_type=`./sqlite3_mips ZKDB.db "SELECT fingerid FROM fptemplate10 WHERE ID = '${finger_id}'"`
+    echo -e "用户: [\033[32;1m${old_name}\033[0m] => [\033[32;1m${new_name}\033[0m] fingerid([\033[32;1m${finger_type}\033[0m])"
+    read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
+    echo "UPDATE fptemplate10 SET pin = '${new_user_defaulf_id}' WHERE ID = '${finger_id}';"
+
+}
+function menu(){
+    echo "==========================="
+    echo "# @Author  : Linsir (root@linsir.org)"
+    echo "# @Link    : http://linsir.org"
+    echo "# @Version : 0.2"
+    echo "Bad option, please choose again"
+    echo "Usage: 1. bash $0 query [Name]: Query user's ID."
+    echo "       2. bash $0 checkin [ID]: Check in for [ID]'s user."
+    echo "       3. bash $0 change: Change checkin time of current month."
+    echo "       4. bash $0 change [time] (2016-06-06T16:03:50): Change checkin time of current month with [time]."
+    echo "       5. bash $0 del [ID]: Delete the [ID]'s checkin log."
+    echo "       6. bash $0 help [ID1] [ID2]: Use [ID1]'finger help [ID2] to checkin.' "
 }
 
 function Usage(){
@@ -116,22 +185,16 @@ function Usage(){
                 del_log $2
                 exit
             ;;
+            "help" )
+                change_finger $2 $3
+                exit
+            ;;
             * )
-            echo "Bad option, please choose again"
-            echo "Usage: 1. bash $0 query [Name]: Query user's ID."
-            echo "       2. bash $0 checkin [ID]: Check in for [ID]'s user."
-            echo "       3. bash $0 change: Change checkin time of current month."
-            echo "       4. bash $0 change [time] (2016-06-06T16:03:50): Change checkin time of current month with [time]."
-            echo "       5. bash $0 del [ID]: Delete the [ID]'s checkin log."
+            menu
             exit
         esac
     done
-    echo "Bad option, please choose again"
-    echo "Usage: 1. bash $0 query [Name]: Query user's ID."
-    echo "       2. bash $0 checkin [ID]: Check in for [ID]'s user."
-    echo "       3. bash $0 change: Change checkin time of current month."
-    echo "       4. bash $0 change [time] (2016-06-06T16:03:50): Change checkin time of current month with [time]."
-    echo "       5. bash $0 del [ID]: Delete the [ID]'s checkin log."
+    menu
 }
 
 Usage "$@"

